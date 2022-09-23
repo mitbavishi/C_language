7c23229867c2d65cb7fec6abaa6f89f151949251
C language <bavishimit@gmail.com>
2022-09-23 19:24:34 +0530

    practical1

---
+++ Ch4/OPR1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+
+ int a,b;
+
+ clrscr();
+
+ printf("Enter x : ");
+ scanf("%d",&x);
+ printf("Enter y : ");
+ scanf("%d",&y);
+
+ printf("Value : %d\n",(x*x) + (2*x*y) + (y*y) );
+ getch();
+
+}
