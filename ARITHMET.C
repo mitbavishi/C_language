eb3ed9f57ac432f76703d2a5360c65da54ddb787
Mit <111973079+mitbavishi@users.noreply.github.com>
2022-10-04 21:13:36 +0530

    Add files via upload

---
+++ ARITHMET.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+   int a = 10;
+   int b = 5;
+   
+   printf("A %d\n",a);
+   printf("B %d\n",b);
+   printf("Sum =%d\n",a+b);
+   printf("Subtraction =%d\n",a-b);
+   printf("Multiplication =%d\n",a*b);
+   printf("Division = %d\n",a%b);
+   printf("Modulos = %d\n",a/b);
+   getch();
+
+
+}
